14a373825aa94cd6ab036885d83edb40dbb5b2be
HBchevelle68 <aportante10@gmail.com>
2023-10-11 12:49:11 -0400

    Added base cli wireframe

--- kbparse/src/main.rs
+++ kbparse/src/main.rs
@@ -1,18 +1,30 @@
-use std::env;
-use std::fs;
-
+use clap::Parser;
 use kbparse_lib::keybag;
+use std::fs;
+use std::path::PathBuf;
 
-// TODO Add cli
-
-// TODO move this to its own crate as a binary crate that depends on the kbparse lib crate
+#[derive(Parser)]
+#[command(
+    version = "0.3.0",
+    about = "Apple Keybag Parser",
+    author = "HBChevelle68"
+)]
+pub struct KBArgs {
+    // #[clap(index = 0)]
+    /// Path to Keybag
+    pub file: PathBuf,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
+    // Parse args
+    let args = KBArgs::parse();
 
-    let bytes = fs::read(&args[1])?;
+    // Resolve the user provided path, error and return if invaild
+    let kb_path = args.file.canonicalize()?;
 
-    let kb = keybag::Keybagv5::new(&bytes)?;
+    let bytes = fs::read(kb_path)?;
+
+    let kb = keybag::Keybagv5::parse(&bytes)?;
 
     dbg!(&kb);
 
