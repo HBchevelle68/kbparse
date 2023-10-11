3c3179061fc23f1549bd4c0e7a651a456e7caee8
HBchevelle68 <aportante10@gmail.com>
2023-10-11 12:59:13 -0400

    fixed up doc errors

--- kbparse-lib/src/lib.rs
+++ kbparse-lib/src/lib.rs
@@ -1,12 +1,14 @@
 //! This crate provides a lightweight ability to parse Apple _user_ keybags (version 5).
 //!
-//! ```rust
+//! ```rust, no_run
 //! // Simplified example
 //!
+//! use kbparse_lib::keybag::Keybagv5;
+//!
 //! fn main() -> Result<(), Box<dyn std::error::Error>> {
-//!     let args: Vec<String> = env::args().collect();
-//!     let bytes = fs::read(&args[1])?;
-//!     let kb = keybag::Keybagv5::new(&bytes)?;
+//!     let args: Vec<String> = std::env::args().collect();
+//!     let bytes = std::fs::read(&args[1])?;
+//!     let kb = Keybagv5::parse(&bytes)?;
 //!     println!("{:#?}", kb);
 //!     dbg!(&kb);
 //!     Ok(())
